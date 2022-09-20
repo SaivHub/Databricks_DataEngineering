6b69e657171b85bb4bf868fbeef1b095215cc44a
SaivHub <SaivHub>
2022-09-20 01:00:28 +0000

    Adding a notebook

---
+++ 00Dev/Notebook Basics.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %python
+# MAGIC print("Hello Databricks. This is Python")
+# MAGIC # Use % to denote the language of run time. It is called as magic command
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "Hello Databricks, This is SQL"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Bridge Business Consulting
+# MAGIC ## Managing Consultant
+# MAGIC **Sai**
+# MAGIC  *Vigneswaran*
+# MAGIC * Can add ordered list, Unordered list, tables, links, images
+
+# COMMAND ----------
+
+# MAGIC %run /Repos/sai.vigneswaran@bridge.consulting/DE_Training/Includes/Classroom-Setup-01.2
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from demo_tmp_vw
